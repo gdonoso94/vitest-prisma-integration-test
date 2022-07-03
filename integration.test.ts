bab9018f592313eda5cba3e359bdae9aa457ce2a
gdonoso <gdonoso94@hotmail.com>
2022-07-03 11:50:02 +0200

    [master]: added workarounds and updated readme

--- integration.test.ts
+++ integration.test.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "./src/generated/client";
-import { describe, expect, it } from "vitest";
+import { describe, it } from "vitest";
 
 describe("Prisma test", () => {
   it("works", () => {
     const prisma = new PrismaClient();
     prisma.$connect();
   });
+
+  // TODO: Add testcontainers and run test against container instance
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,20 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import { resolve } from "path";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: "./src/generated/client",
+        replacement: resolve(__dirname, "./src/generated/client/index.js"),
+      },
+    ],
+  },
+  plugins: [tsconfigPaths()],
   test: {
     hookTimeout: 50000,
     globals: true,
